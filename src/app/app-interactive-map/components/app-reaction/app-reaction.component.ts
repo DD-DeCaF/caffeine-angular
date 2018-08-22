8922ef419bc714467a46eb0ad05e2392c46f6aae
Mátyás Fodor <matyas.fodor@gmail.com>
2018-08-22 17:22:48 +0200

    chore: remove Primitive type

--- src/app/app-interactive-map/components/app-reaction/app-reaction.component.ts
+++ src/app/app-interactive-map/components/app-reaction/app-reaction.component.ts
@@ -17,7 +17,6 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-reaction',
   templateUrl: './app-reaction.component.html',
-  styleUrls: ['./app-reaction.component.scss'],
 })
 export class AppReactionComponent {
 }
