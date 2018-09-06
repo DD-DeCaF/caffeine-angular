69f10b9b3eb49a78c63d55ad9d7519b695f0b989
AlbaLopez <alba.lopez.benito@gmail.com>
2018-09-06 09:22:05 +0200

    fix: default map to set the first one if the model has maps.

--- src/app/app-interactive-map/store/interactive-map.reducer.spec.ts
+++ src/app/app-interactive-map/store/interactive-map.reducer.spec.ts
@@ -180,4 +180,12 @@ describe('interactiveMapReducer', () => {
       direction: 'min',
     });
   });
+
+  it('should set the selected specie', () => {
+    const state = interactiveMapReducer(
+      undefined,
+      new fromActions.SetSelectedSpecies({project_id: null, id: '2', name: 'Escherichia coli', created: '2018-07-04T15:22:05.701458', updated: null}),
+    );
+    expect(state.selectedSpecies).toEqual({project_id: null, id: '2', name: 'Escherichia coli', created: '2018-07-04T15:22:05.701458', updated: null});
+  });
 });
