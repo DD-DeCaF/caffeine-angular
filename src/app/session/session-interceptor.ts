05e5fe9af63c5d2266a986a50ca17f8ab65c17ff
Ali Kaafarani <ali@kvikshaug.no>
2018-10-23 18:59:24 +0200

    feat: refresh JWT if expired

--- src/app/session/session-interceptor.ts
+++ src/app/session/session-interceptor.ts
@@ -16,32 +16,56 @@ import {Injectable, Injector} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 
 import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
+import {tap, switchMap} from 'rxjs/operators';
 
 import {SessionService} from './session.service';
 import {AUTHORIZATION_TOKEN} from './consts';
 
 @Injectable()
 export class SessionInterceptor implements HttpInterceptor {
+  private refresh$;
+
   constructor(private injector: Injector) {
   }
   // tslint:disable-next-line:no-any
   public intercept(req: HttpRequest<Request>, next: HttpHandler): Observable<HttpEvent<any>> {
     const session = this.injector.get(SessionService);
 
-    if (session.hasToken() && session.isTrustedURL(req.url)) {
-      const jwt = localStorage.getItem(AUTHORIZATION_TOKEN);
-      req = req.clone({headers: req.headers.set('Authorization', `Bearer ${jwt}`)});
+    // Don't intercept the actual JWT refresh request.
+    if(session.isRefreshURL(req.url)) {
+      return next.handle(req);
     }
 
-    return next.handle(req).pipe(
-      tap(
-        () => { /* no-empty */ },
-        (response) => {
-          if (response.status === 401) {
-            session.logout();
+    if (session.hasToken() && session.isTrustedURL(req.url)) {
+      // Add the authorization header from token in local storage.
+      const authorizedRequest = () => {
+        const jwt = localStorage.getItem(AUTHORIZATION_TOKEN);
+        req = req.clone({headers: req.headers.set('Authorization', `Bearer ${jwt}`)});
+        return next.handle(req);
+      }
+
+      if(this.refresh$ !== undefined) {
+        // If a refresh request is currently in progress, wait for the token to refresh before proceeding with the
+        // original request.
+        return this.refresh$.pipe(switchMap(authorizedRequest));
+      } else if (session.authorizationExpired()) {
+        // If the JWT has expired, refresh it before continuing.
+        this.refresh$ = session.refresh();
+        this.refresh$.subscribe({
+          complete: () => {
+            // On completion, unset the refresh observable so subsequent requests stop attaching to it.
+            this.refresh$ = undefined;
           }
-        },
-      ));
+        });
+        // Now wait for the refreshed token to arrive before proceeding with the original request.
+        return this.refresh$.pipe(switchMap(authorizedRequest));
+      } else {
+        // Session is still valid - simply handle the request with autorization header added.
+        return authorizedRequest();
+      }
+    }
+
+    // Authorization header is not relevant; handle request normally.
+    return next.handle(req);
   }
 }
