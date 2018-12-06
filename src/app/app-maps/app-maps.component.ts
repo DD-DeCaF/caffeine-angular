90554f243a16fb06e37df7aee747f60fdc6edf02
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2018-12-06 22:54:05 +0100

    feat: add pagination to models and maps

--- src/app/app-maps/app-maps.component.ts
+++ src/app/app-maps/app-maps.component.ts
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatTableDataSource, MatSort, MatDialog} from '@angular/material';
+import {MatTableDataSource, MatSort, MatDialog, MatPaginator} from '@angular/material';
 import {AppState} from '../store/app.reducers';
 import {select, Store} from '@ngrx/store';
 import * as fromActions from './store/maps.actions';
@@ -36,6 +36,7 @@ export class AppMapsComponent implements OnInit {
   public models: Observable<types.DeCaF.ModelHeader[]>;
   public sessionState: Observable<SessionState>;
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   displayedColumns: string[] = [
     'name',
@@ -54,9 +55,13 @@ export class AppMapsComponent implements OnInit {
     this.store.pipe(select((store) => store.shared.maps)).subscribe((maps) => {
       this.dataSource.data = maps;
     });
-    this.dataSource.sort = this.sort;
+    this.dataSource.sort = this.sort; 
     this.models = this.store.pipe(select((store) => store.shared.modelHeaders));
     this.sessionState = this.store.select('session');
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
   }
 
   removeMap(map: string): void {
--- src/app/app-models/app-models.component.ts
+++ src/app/app-models/app-models.component.ts
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatTableDataSource, MatSort, MatDialog} from '@angular/material';
+import {MatTableDataSource, MatSort, MatDialog, MatPaginator} from '@angular/material';
 import {AppState} from '../store/app.reducers';
 import {select, Store} from '@ngrx/store';
 import * as fromActions from './store/models.actions';
@@ -35,6 +35,7 @@ export class AppModelsComponent implements OnInit {
   public sessionState: Observable<SessionState>;
 
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   displayedColumns: string[] = [
     'id',
@@ -54,6 +55,10 @@ export class AppModelsComponent implements OnInit {
     });
     this.dataSource.sort = this.sort;
     this.sessionState = this.store.select('session');
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
   }
 
   removeModel(model: string): void {
