d6e9165e232f56062bf2aa969bfb158cc49e4d43
Ali Kaafarani <ali@kvikshaug.no>
2019-02-20 10:26:27 +0100

    style: ignore script

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,7 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err) => console.log(err));
 
 if (environment.hotjar) {
+  // tslint:disable-next-line
   const script = "(function (h, o, t, j, a, r) { h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) }; h._hjSettings = { hjid: 1194839, hjsv: 6 }; a = o.getElementsByTagName('head')[0]; r = o.createElement('script'); r.async = 1; r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv; a.appendChild(r); })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');";
   document.write(`<script>${script}</script>`);
 }
