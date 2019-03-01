7c0e0a7b12c6a3352a9b2cf8fe37d259ee978337
Anna Sirunian <anna_sirunian@ukr.net>
2019-03-01 13:31:06 +0100

    feat: disable google analytics for local configuration

--- src/main.ts
+++ src/main.ts
@@ -30,3 +30,11 @@ if (environment.hotjar) {
   const script = "(function (h, o, t, j, a, r) { h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) }; h._hjSettings = { hjid: 1194839, hjsv: 6 }; a = o.getElementsByTagName('head')[0]; r = o.createElement('script'); r.async = 1; r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv; a.appendChild(r); })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');";
   document.write(`<script>${script}</script>`);
 }
+
+if (environment.GA) {
+  const gaScript = `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');`;
+  document.write(`<script>${gaScript}</script>`);
+}
